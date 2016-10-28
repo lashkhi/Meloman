b9c7da3e003b1c7b580d5f71daf4fd964d30e665
David Lashkhi <davidlashkhi@Davids-MacBook-Pro.local>
2016-10-28 23:31:25 +0300

    Final player

--- Meloman/Meloman/ViewController.swift
+++ Meloman/Meloman/ViewController.swift
@@ -12,12 +12,11 @@ import AVKit
 
 
 private var playerVCKVOContext = 0
+let urlString = "http://www.listenlive.eu/bbcradio1.m3u"
 
 
 class ViewController: UIViewController {
-    // MARK: Properties
     
-    // Attempt load and test these asset keys before playing.
     static let assetKeysRequiredToPlay = [
         "playable",
         "hasProtectedContent"
@@ -39,58 +38,29 @@ class ViewController: UIViewController {
     var asset: AVURLAsset? {
         didSet {
             guard let newAsset = asset else { return }
-            
             asynchronouslyLoadURLAsset(newAsset)
         }
     }
     
-    private var playerLayer: AVPlayerLayer? {
-        return playerView.playerLayer
-    }
-    
-    
-    /*
-     A token obtained from calling `player`'s `addPeriodicTimeObserverForInterval(_:queue:usingBlock:)`
-     method.
-     */
     
     private var playerItem: AVPlayerItem? = nil {
         didSet {
-            /*
-             If needed, configure player item here before associating it with a player.
-             (example: adding outputs, setting text style rules, selecting media options)
-             */
+
             player.replaceCurrentItem(with: self.playerItem)
         }
     }
     
-    // MARK: - IBOutlets
     
     @IBOutlet weak var playPauseButton: UIButton!
-    @IBOutlet weak var playerView: PlayerView!
     
-    // MARK: - View Controller
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        /*
-         Update the UI when these player properties change.
-         
-         Use the context parameter to distinguish KVO for our particular observers
-         and not those destined for a subclass that also happens to be observing
-         these properties.
-         */
-        
         addObserver(self, forKeyPath: #keyPath(ViewController.player.rate), options: [.new, .initial], context: &playerVCKVOContext)
         
-        
-        playerView.playerLayer.player = player
-        
-        let movieURL = URL(string: "http://www.listenlive.eu/bbcradio1.m3u")!
-        asset = AVURLAsset(url: movieURL, options: nil)
-        
-        // Make sure we don't have a strong reference cycle by only capturing self as weak.
+        let radioURL = URL(string: urlString)!
+        asset = AVURLAsset(url: radioURL, options: nil)
         
         player.play()
     }
@@ -104,37 +74,17 @@ class ViewController: UIViewController {
         
     }
     
-    // MARK: - Asset Loading
     
     func asynchronouslyLoadURLAsset(_ newAsset: AVURLAsset) {
-        /*
-         Using AVAsset now runs the risk of blocking the current thread (the
-         main UI thread) whilst I/O happens to populate the properties. It's
-         prudent to defer our work until the properties we need have been loaded.
-         */
+        
         newAsset.loadValuesAsynchronously(forKeys: ViewController.assetKeysRequiredToPlay) {
-            /*
-             The asset invokes its completion handler on an arbitrary queue.
-             To avoid multiple threads using our internal state at the same time
-             we'll elect to use the main thread at all times, let's dispatch
-             our handler to the main queue.
-             */
             DispatchQueue.main.async {
-                /*
-                 `self.asset` has already changed! No point continuing because
-                 another `newAsset` will come along in a moment.
-                 */
                 guard newAsset == self.asset else { return }
-                
-                /*
-                 Test whether the values of each of the keys we need have been
-                 successfully loaded.
-                 */
                 for key in ViewController.assetKeysRequiredToPlay {
                     var error: NSError?
                     
                     if newAsset.statusOfValue(forKey: key, error: &error) == .failed {
-                        let stringFormat = NSLocalizedString("error.asset_key_%@_failed.description", comment: "Can't use this AVAsset because one of it's keys failed to load")
+                        let stringFormat = "Can't use this AVAsset because one of it's keys failed to load"
                         
                         let message = String.localizedStringWithFormat(stringFormat, key)
                         
@@ -144,63 +94,45 @@ class ViewController: UIViewController {
                     }
                 }
                 
-                // We can't play this asset.
                 if !newAsset.isPlayable || newAsset.hasProtectedContent {
-                    let message = NSLocalizedString("error.asset_not_playable.description", comment: "Can't use this AVAsset because it isn't playable or has protected content")
+                    let message = "Can't use this AVAsset because it isn't playable or has protected content"
                     
                     self.handleErrorWithMessage(message)
                     
                     return
                 }
                 
-                /*
-                 We can play this asset. Create a new `AVPlayerItem` and make
-                 it our player's current item.
-                 */
                 self.playerItem = AVPlayerItem(asset: newAsset)
             }
         }
     }
     
-    // MARK: - IBActions
-    
     @IBAction func playPauseButtonWasPressed(_ sender: UIButton) {
         if player.rate != 1.0 {
-            // Not playing forward, so play.
-            
             player.play()
         }
         else {
-            // Playing, so pause.
             player.pause()
         }
     }
     
     
     // MARK: - KVO Observation
-    
-    // Update our UI when player or `player.currentItem` changes.
     override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey: Any]?, context: UnsafeMutableRawPointer?) {
-        // Make sure the this KVO callback was intended for this view controller.
         guard context == &playerVCKVOContext else {
             super.observeValue(forKeyPath: keyPath, of: object, change: change, context: context)
             return
         }
         
         if keyPath == #keyPath(ViewController.player.rate) {
-            // Update `playPauseButton` image.
             
             let newRate = (change?[NSKeyValueChangeKey.newKey] as! NSNumber).doubleValue
-            
             let buttonImageName = newRate == 1.0 ? "Pause" : "Play"
-            
             let buttonImage = UIImage(named: buttonImageName)
-            
             playPauseButton.setImage(buttonImage, for: UIControlState())
         }
     }
     
-    // Trigger KVO for anyone observing our properties affected by player and player.currentItem
     override class func keyPathsForValuesAffectingValue(forKey key: String) -> Set<String> {
         let affectedKeyPathsMappingByKey: [String: Set<String>] = [
             "rate":         [#keyPath(ViewController.player.rate)]
@@ -214,12 +146,12 @@ class ViewController: UIViewController {
     func handleErrorWithMessage(_ message: String?, error: Error? = nil) {
         NSLog("Error occured with message: \(message), error: \(error).")
         
-        let alertTitle = NSLocalizedString("alert.error.title", comment: "Alert title for errors")
-        let defaultAlertMessage = NSLocalizedString("error.default.description", comment: "Default error message when no NSError provided")
+        let alertTitle = "Error"
+        let defaultAlertMessage = "Something went wrong"
         
         let alert = UIAlertController(title: alertTitle, message: message == nil ? defaultAlertMessage : message, preferredStyle: UIAlertControllerStyle.alert)
         
-        let alertActionTitle = NSLocalizedString("alert.error.actions.OK", comment: "OK on error alert")
+        let alertActionTitle = "OK"
         
         let alertAction = UIAlertAction(title: alertActionTitle, style: .default, handler: nil)
         
